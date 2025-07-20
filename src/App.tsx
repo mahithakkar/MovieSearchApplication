2a520a83d2a91ac17ab9bed39296a8386be84d32
Hita Thota <hita@thethotas.com>
2025-07-20 19:55:51 -0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import DisplayPage from './pages/DisplayPage';
-import DebuggingPage from './pages/DebuggingPage';
-import './App.css';
-
-function App() {
+const App = () => {
+  // Replace this with your code. Good luck!
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/search" element={<DisplayPage />} />
-        <Route path="/debugging" element={<DebuggingPage />} />
-      </Routes>
-    </>
+    <div>
+      <h1>Fall 2025 Hack4Impact-UMD Technical Application Assessment</h1>
+    </div>
   );
-}
+};
 
 export default App;
