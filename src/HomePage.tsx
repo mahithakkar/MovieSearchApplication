ebc7f241267368161feb58d59c85f04d122628a1
Mahi Thakkar <mahigthakkar2005@gmail.com>
2025-08-19 13:06:47 -0400

    changes with home

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -4,6 +4,13 @@ export default function HomePage()
         //tagging the style element so later we can write css
         <div className= "homepage">
             <h1>Movie Search</h1>
+
+             {/* simple form; no JS logic yet */}
+
+             <form>
+                <input name= "q" placeholder = "Search for a title..." />
+             </form>
+        
         </div>
-    );
+    ); 
 }
