9581fb9fe009487a8539b1801d01831d8ba98c21
Mahi Thakkar <mahigthakkar2005@gmail.com>
2025-08-21 19:58:53 -0400

    fixed rendering- app and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+//router lives here
+
+//import functions from router dom
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./HomePage";
 import DisplayPage from "./DisplayPage";
 
+//made path and element so the page doesnt ahve to reload each time, sets path
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/search", element: <DisplayPage /> },
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,13 @@
+//only renders app so here it renders to app itslef, then app handles all the navigation to 
+//display and home page
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-
-import HomePage from "./HomePage";
-import DisplayPage from "./DisplayPage";
-
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/search", element: <DisplayPage /> },
-]);
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
